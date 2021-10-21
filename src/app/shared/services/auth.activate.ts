bbf40ff7718c3d2dbe5008feb2fb18daaad56579
Atanas Ivanov <softuni.angular.exam@gmail.com>
2021-10-22 00:37:30 +0300

    aut service unsb

--- src/app/shared/services/auth.activate.ts
+++ src/app/shared/services/auth.activate.ts
@@ -1,25 +1,33 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+
+import { User } from '../models/user.model';
 
 @Injectable({providedIn: 'root'})
 export class AuthActivate implements CanActivate {
-  isAuth!: boolean;
-
+  user!: User;
+  userSubscription!: Subscription;
+  
   constructor(
     private _router: Router,
     private _firebaseAuth: AngularFireAuth
   ){}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    const { autenticationRequired, autenticationFailureRedirectUrl } = route.data;
+    const { autenticationRequired, autenticationFailureRedirectUrl, role } = route.data;
 
     return new Promise((resolve, reject) => {
-      this._firebaseAuth.authState.subscribe((user) => {
+      this.userSubscription = this._firebaseAuth.authState.subscribe((user) => {
+        console.log('11');
           if (autenticationRequired && user || !autenticationRequired && !user) {
+            console.log('22');
+            this.userSubscription.unsubscribe();
             return resolve(true);
           }
+          console.log('33');
+          this.userSubscription.unsubscribe();
           return this._router.navigate([autenticationFailureRedirectUrl]);
         }
       )
