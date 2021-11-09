45ab808e5618e72338a4a4264722d658eaf968da
Atanas Ivanov <softuni.angular.exam@gmail.com>
2021-11-09 22:34:41 +0200

    styles

--- src/app/control-panel/settings/roles.service.ts
+++ src/app/control-panel/settings/roles.service.ts
@@ -7,7 +7,7 @@ export class RolesService {
   defaultRoles: Array<{name: string, value: string, route: string, checked: boolean}> = [
     {name: 'Users', value: 'users', route: 'users', checked: false},
     {name: 'Register', value: 'register', route: 'register', checked: false},
-    {name: 'Blocked Users', value: 'blockedUsers', route: 'blocked-users', checked: false},
+    {name: 'Blocked Users', value: 'blockedUsers', route: 'blocked-users', checked: true},
     {name: 'Account Settings', value: 'accountSettings', route: 'account-settings', checked: true},
     {name: 'Domain Options', value: 'domainOptions', route: 'domain-options', checked: false},
   ];
