dc80ce82df641a1546c4eca1a6316cfe38da2e20
Atanas Ivanov <softuni.angular.exam@gmail.com>
2021-11-12 17:59:28 +0200

    styles

--- src/app/shared/chat-form-history/chat-form-history.component.ts
+++ src/app/shared/chat-form-history/chat-form-history.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Chat, ChatHistiry } from '../models/chat.model';
 
 @Component({
@@ -8,6 +8,7 @@ import { Chat, ChatHistiry } from '../models/chat.model';
 })
 export class ChatFormHistoryComponent implements OnInit {
   @Input() chatHistory: ChatHistiry[] | undefined = [];
+  @ViewChild('chatHistoryContainer') chatHistoryContainer!: ElementRef<HTMLElement>;
 
   constructor() { }
 
