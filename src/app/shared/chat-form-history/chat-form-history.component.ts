24441935012304e10d2a7130700a73085077857f
Atanas Ivanov <softuni.angular.exam@gmail.com>
2021-11-13 09:47:52 +0200

    styles

--- src/app/shared/chat-form-history/chat-form-history.component.ts
+++ src/app/shared/chat-form-history/chat-form-history.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Chat, ChatHistiry } from '../models/chat.model';
 
 @Component({
@@ -6,12 +6,17 @@ import { Chat, ChatHistiry } from '../models/chat.model';
   templateUrl: './chat-form-history.component.html',
   styleUrls: ['./chat-form-history.component.scss']
 })
-export class ChatFormHistoryComponent implements OnInit {
+export class ChatFormHistoryComponent implements OnInit, AfterViewInit {
   @Input() chatHistory: ChatHistiry[] | undefined = [];
-  @ViewChild('chatHistoryContainer') chatHistoryContainer!: ElementRef<HTMLElement>;
+  @ViewChild('chatHistoryContainer') chatHistoryContainer!: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {   
+  ngOnInit(): void {
+
+  };
+
+  ngAfterViewInit(): void {
+    this.chatHistoryContainer.nativeElement.scrollTop = this.chatHistoryContainer.nativeElement.scrollHeight;
   };
 }
