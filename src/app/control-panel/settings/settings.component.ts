133d0d8bc96de15d57102ff7b1c27c9da87de4b6
Atanas Ivanov <softuni.angular.exam@gmail.com>
2021-11-13 18:58:56 +0200

    save

--- src/app/control-panel/settings/settings.component.ts
+++ src/app/control-panel/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { User } from 'src/app/shared/models/user.model';
@@ -10,17 +11,21 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit, OnDestroy {
-  user!: User;
+  user: User = undefined!;
   userSubscirption!: Subscription;
 
   constructor(
-    private _authService: AuthService
+    private _authService: AuthService,
+    private _router: Router
   ) { }
 
   ngOnInit(): void {
     this.userSubscirption = this._authService.user.subscribe(user => {
-      this.user = user;      
-    });    
+      this.user = user;
+      if (this.user != undefined) {        
+        this._router.navigate(['/control-panel/settings/users']);
+      };
+    });   
   }
 
   ngOnDestroy(): void {
